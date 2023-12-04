34c639803c96af0e263b49e5f0036b4a57b5db72
Valmir Almeida <almeidavalmir76@gmail.com>
2023-12-04 18:32:12 -0300

    Add delete route & add  vehicle with photos

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
-const app = express();
 const { MongoClient, ObjectId } = require("mongodb");
 const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
 require("dotenv").config();
+
+const app = express();
 const port = 3000;
 
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
-
 app.use(bodyParser.json());
+
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
 app.get("/vehicles", async (req, res) => {
   try {
     await client.connect();
@@ -54,8 +71,13 @@ app.get("/vehicles/:id", async (req, res) => {
   }
 });
 
-app.post("/vehicles", async (req, res) => {
+app.post("/vehicles", upload.array("images", 12), async (req, res) => {
   const newVehicle = req.body;
+  const images = req.files.map((file) => file.filename);
+
+  if (!newVehicle.placa) {
+    return res.status(400).send({ message: "A placa é obrigatória" });
+  }
 
   try {
     await client.connect();
@@ -70,6 +92,8 @@ app.post("/vehicles", async (req, res) => {
     if (existingVehicleId || existingVehiclePlaca) {
       res.status(400).send({ message: "Veículo já cadastrado" });
     } else {
+      newVehicle.images = images;
+
       const result = await collection.insertOne(newVehicle);
       res.status(201).send({ message: "Veículo adicionado com sucesso" });
     }
@@ -110,6 +134,45 @@ app.put("/vehicles/:id", async (req, res) => {
       res.send({ message: "Veículo atualizado com sucesso" });
     } else {
       res.send({ message: "Nenhuma alteração realizada" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Erro interno do servidor");
+  } finally {
+    await client.close();
+  }
+});
+
+app.delete("/vehicles/:id", async (req, res) => {
+  const vehicleId = req.params.id;
+
+  if (!vehicleId) {
+    return;
+  }
+
+  try {
+    await client.connect();
+    const database = client.db("Revenda");
+    const collection = database.collection("vehicles");
+
+    const objectId = ObjectId.isValid(vehicleId)
+      ? new ObjectId(vehicleId)
+      : vehicleId;
+
+    const existingVehicle = await collection.findOne({
+      _id: objectId,
+    });
+
+    if (!existingVehicle) {
+      res.status(404).send({ message: "Veículo não encontrado" });
+    }
+
+    const result = await collection.deleteOne({ _id: objectId });
+
+    if (result.deletedCount > 0) {
+      res.send({ message: "Veículo excluído com sucesso" });
+    } else {
+      res.send({ message: "Nenhum veículo excluído" });
     }
   } catch (error) {
     console.log(error);
