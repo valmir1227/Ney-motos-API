37905fa83485941bea021028d423410dd6766d76
Valmir Almeida <almeidavalmir76@gmail.com>
2024-04-20 10:58:37 -0300

    change vendaController schema

--- src/controllers/vendaController.js
+++ src/controllers/vendaController.js
@@ -3,31 +3,26 @@ const Venda = require("../models/vendaSchema");
 const Estoque = require("../models/estoqueSchema");
 
 exports.createVenda = async (req, res) => {
-  const { id_cliente, id_moto, valor_venda, forma_pagamento, status } =
-    req.body;
+  const { id_moto, valor_venda } = req.body;
 
   try {
     const motoEstoque = await Estoque.findById(id_moto);
 
     const venda = new Venda({
-      id_cliente,
       data_venda: new Date(),
       valor_venda,
-      forma_pagamento,
-      status,
       moto: {
         id_moto: motoEstoque._id,
         marca: motoEstoque.marca,
-        status: motoEstoque.status,
+        status:"Vendido",
         modelo: motoEstoque.modelo,
         ano_fabricacao: motoEstoque.ano_fabricacao,
         ano_modelo: motoEstoque.ano_modelo,
         tipo_moto: motoEstoque.tipo_moto,
         cor: motoEstoque.cor,
         quilometragem: motoEstoque.quilometragem,
-        preco_compra: motoEstoque.preco_compra,
+        preco: motoEstoque.preco,
         data_entrada: motoEstoque.data_entrada,
-        extras: motoEstoque.extras,
         imagens: motoEstoque.imagens,
       },
     });
@@ -35,7 +30,7 @@ exports.createVenda = async (req, res) => {
     await venda.save();
     await Estoque.findByIdAndDelete(id_moto);
 
-    res.status(201).json({ message: "Venda realizada com sucesso" });
+    res.status(201).json({ message: "Venda cadastrada com sucesso" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Erro ao cadastrar venda" });
@@ -70,35 +65,6 @@ exports.getVendaById = async (req, res) => {
   } catch (error) {
     console.error("Erro ao acessar o MongoDB:", error);
     res.status(500).json({ message: "Erro interno do servidor" });
-  }
-};
-
-exports.updateVenda = async (req, res) => {
-  const vendaId = req.params.id;
-  const updateVendaData = req.body;
-
-  try {
-    if (!mongoose.isValidObjectId(vendaId)) {
-      return res.status(400).send({ message: "ID inválido" });
-    }
-
-    const updatedVenda = await Venda.findByIdAndUpdate(vendaId, {
-      $set: {
-        data_venda: updateVendaData.data_venda,
-        valor_venda: updateVendaData.valor_venda,
-        forma_pagamento: updateVendaData.forma_pagamento,
-        status: updateVendaData.status,
-      },
-    });
-
-    if (updatedVenda) {
-      res.send({ message: "Dados da venda atualizado com sucesso" });
-    } else {
-      res.status(404).send({ message: "Registro de venda não encontrado" });
-    }
-  } catch (error) {
-    console.error("Erro ao atualizar dados da venda:", error);
-    res.status(500).send({ message: "Erro interno do servidor" });
   }
 };
 
--- src/models/vendaSchema.js
+++ src/models/vendaSchema.js
@@ -1,11 +1,9 @@
 const mongoose = require("mongoose");
 
 const vendaSchema = new mongoose.Schema({
-  id_cliente: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Clients",
-    required: true,
-  },
+  data_venda: { type: Date, default: Date.now },
+  valor_venda: { type: Number, required: true },
+
   moto: {
     type: {
       marca: { type: String, required: true },
@@ -15,26 +13,18 @@ const vendaSchema = new mongoose.Schema({
       tipo_moto: { type: String, required: true },
       cor: { type: String, required: true },
       quilometragem: { type: Number, required: true },
-      preco_compra: { type: Number, required: true },
+      preco: { type: Number, required: true },
       data_entrada: { type: Date, required: true },
+      status: {
+        type: String,
+        required: true,
+      },
       capacidade_motor: { type: String },
       tipo_combustivel: { type: String },
       sistema_freios: { type: String },
-      extras: { type: [String] },
-      descricao: { type: String },
-      observacao: { type: String },
-      status: { type: String, required: true },
       imagens: { type: [String], required: true },
     },
     required: true,
-  },
-  data_venda: { type: Date, default: Date.now },
-  valor_venda: { type: Number, required: true },
-  forma_pagamento: { type: String, required: true },
-  status: {
-    type: String,
-    enum: ["Concluída", "Pendente", "Cancelada", "Parcelado"],
-    default: "Concluída",
   },
 });
 
